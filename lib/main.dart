ad965e3993ac49a2f508ce3fd8559db69934c6f9
tanduyfcsc <tanduyfcsc@gmail.com>
2024-08-12 22:04:35 +0700

    Continue with the pictures and reviews

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -57,23 +58,91 @@ class _MyHomePageState extends State<MyHomePage> {
       body: PageView(
         controller: _pageController,
         children: [
-          Container(
-            color: Colors.blue,
-          ),
-          Container(
-            color: Colors.amber,
-          ),
-          Container(
-            color: Colors.green,
-          ),
+          makePage(image: 'assets/images/one.jpg'),
+          makePage(image: 'assets/images/three.jpg'),
+          makePage(image: 'assets/images/two.jpg'),
         ],
       ),
     );
   }
 
-  Widget makePage(){
+  Widget makePage({image}){
     return Container(
-      color: Colors.amber,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(image),
+          fit: BoxFit.cover
+        )
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: [0.08,0.9],
+            colors: [
+              Colors.black.withOpacity(0.9),
+              Colors.black.withOpacity(0.2),
+            ]
+          )
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text('1', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
+                  Text('/3', style: TextStyle(color: Colors.white, fontSize: 15),)
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('USA', style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star, color: Colors.yellow,size: 15,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star, color: Colors.yellow,size: 15,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star, color: Colors.yellow,size: 15,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star, color: Colors.yellow,size: 15,),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(Icons.star, color: Colors.grey,size: 15,),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
